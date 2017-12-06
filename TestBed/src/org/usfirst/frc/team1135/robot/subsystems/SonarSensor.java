edcd001772647070248dfdfb1501dcb3008be202
Evan Smith <esmith25@phm.k12.in.us>
2017-12-06 15:13:41 -0500

    i am still david

--- TestBed/src/org/usfirst/frc/team1135/robot/subsystems/SonarSensor.java
+++ TestBed/src/org/usfirst/frc/team1135/robot/subsystems/SonarSensor.java
@@ -1,18 +1,23 @@
 package org.usfirst.frc.team1135.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import java.lang.*;
 
 /**
  *
  */
 public class SonarSensor extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+  static final int SONAR_PING_PORT = 9;
+  static final int SONAR_ECHO_PORT = 8;
+  //static final double DEGREES_TI_RADIANS_CONSTANT = (M_PI/180.0)
+  int distance = 0;
+  
+  
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+       
+    	
+    	
     }
 }
 
