f6efebbf6e5c70b1df6d8caee1326efdaa6b782a
robot <robot@10.102.32.101>
2017-12-20 17:05:22 -0500

    Beta!

---
+++ TestBed/src/org/usfirst/frc/team135/robot/OI.java
@@ -0,0 +1,123 @@
+package org.usfirst.frc.team135.robot;
+
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.Joystick;
+import java.lang.Math;
+
+import org.usfirst.frc.team135.robot.commands.DriveStraight;
+import org.usfirst.frc.team135.robot.commands.ShiftGears;
+import org.usfirst.frc.team135.robot.commands.StopShift;
+import org.usfirst.frc.team135.robot.commands.UnshiftGears;
+
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	/*public final int MAX_NUMBER_JOYSTICKS = 2;
+	public final int MAX_NUMBER_BUTTONS = 12;
+	
+	public final int LEFT_JOYSTICK = 0;
+	public final int RIGHT_JOYSTICK = 1;
+	
+	public final int ENGAGE_SHIFTER_BUTTON = 7; //trigger
+	public final int DISENGAGE_SHIFTER_BUTTON = 8;
+	public boolean gearShiftOn = false; */
+	
+	public final double JOYSTICK_DEADBAND = 0.15;
+	
+	/*public Joystick[] joystick;
+	public JoystickButton[][] button;*/
+	
+	Joystick RIGHT, LEFT, MANIP;
+	JoystickButton ENGAGE, DISENGAGE, ZERO;
+	
+	JoystickButton DRIVE_STRAIGHT;
+	
+	public static OI instance;
+	
+public static OI getInstance()
+{		
+	if (instance == null)		
+	{
+		instance = new OI();
+	}
+	return instance;
+}
+
+private OI()
+{
+	/*joystick = new Joystick[MAX_NUMBER_JOYSTICKS];
+	button = new JoystickButton[MAX_NUMBER_JOYSTICKS][MAX_NUMBER_BUTTONS];
+	InitializeJoysticks();
+	ConfigureButtonMapping();*/
+	RIGHT = new Joystick(1);
+	LEFT = new Joystick(0);
+	MANIP = new Joystick(2);
+	
+	ConfigureButtonMapping();
+
+}
+
+/*public void InitializeJoysticks()
+{
+
+	for (int i=0;i <=MAX_NUMBER_JOYSTICKS; i++)
+	{
+		joystick[i] = new Joystick(i);
+		for (int k=0; k<=MAX_NUMBER_BUTTONS; k++)
+		{
+			button[i][k] = new JoystickButton(joystick[i], k);
+		}
+	}
+}
+*/
+/*public boolean GetButton(int joysticknumber, int buttonnumber)
+{
+	boolean value = button[joysticknumber][buttonnumber].get();
+	return value;
+}*/
+public double SetThreshold(double joystickValue)
+{
+	if (Math.abs(joystickValue) <= JOYSTICK_DEADBAND)
+	{
+		return 0;
+	}
+	else 
+	{
+		return joystickValue;
+	}
+}
+
+public double GetLeftJoystickY()
+{
+	double value = SetThreshold(LEFT.getY());
+	return value;
+}
+
+public double GetRightJoystickY()
+{
+	double value = SetThreshold(RIGHT.getY());
+	return value;
+
+	
+}
+
+public void ConfigureButtonMapping()
+{
+	ENGAGE = new JoystickButton(MANIP, 7);
+	DISENGAGE = new JoystickButton(MANIP, 8);
+	ZERO = new JoystickButton(MANIP, 10);
+	DRIVE_STRAIGHT = new JoystickButton(RIGHT, 5);
+	AssignButtons();
+}
+
+public void AssignButtons()
+{
+	DRIVE_STRAIGHT.whileHeld(new DriveStraight());
+	DISENGAGE.whenPressed(new UnshiftGears());
+	ENGAGE.whenPressed(new ShiftGears());
+	ZERO.whenPressed(new StopShift());
+}
+}
---
+++ TestBed/src/org/usfirst/frc/team135/robot/subsystems/DriveTrain.java
@@ -0,0 +1,150 @@
+package org.usfirst.frc.team135.robot.subsystems;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.usfirst.frc.team135.robot.RobotMap;
+import org.usfirst.frc.team135.robot.commands.DriveJ;
+import org.usfirst.frc.team135.robot.wrappers.pid_DifferentialDrive;
+import org.usfirst.frc.team135.robot.wrappers.pid_DifferentialDrive.*;
+
+import edu.wpi.first.wpilibj.*;
+
+import com.ctre.phoenix.MotorControl.ControlMode;
+import com.ctre.phoenix.MotorControl.FeedbackDevice;
+import com.ctre.phoenix.MotorControl.StatusFrameEnhanced;
+import com.ctre.phoenix.MotorControl.CAN.TalonSRX;
+
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem implements RobotMap {
+	
+	private static final int NUMBER_DRIVETRAIN_MOTORS = 4;
+	private static final int FRONT_LEFT = 0;
+	private static final int REAR_LEFT = 1;
+	private static final int FRONT_RIGHT = 2;
+	private static final int REAR_RIGHT = 3;
+
+	public static final int LEFT_ENCODER = FRONT_LEFT;
+	public static final int RIGHT_ENCODER = FRONT_RIGHT;
+	
+	//-1 is reversed. 1 is not reversed.
+	private static final int LEFT_ENCODER_REVERSED = -1;
+	private static final int RIGHT_ENCODER_REVERSED = -1;
+	
+	
+	private static TalonSRX[] drivetrainMotors;
+	
+	private static final int ENCODER_COUNT = 256;
+	private static final int QUAD_COUNT = (ENCODER_COUNT * 4);
+	
+	
+	public pid_DifferentialDrive chassis;
+	public ADXRS450_Gyro gyro;
+	
+	public PIDController straightController;
+
+	private static DriveTrain instance;
+
+
+public void initDefaultCommand() 
+{
+    setDefaultCommand(new DriveJ());
+    }
+
+public static DriveTrain getInstance()
+{
+	if (instance == null)
+	{
+		instance = new DriveTrain();
+	}
+	return instance;
+}
+
+private DriveTrain()
+{
+	//InitializeDriveTrainMotors();
+	drivetrainMotors = new TalonSRX[NUMBER_DRIVETRAIN_MOTORS];
+
+	gyro = new ADXRS450_Gyro(); //Gyro is connected to the onboard SPI on line 0
+	
+	drivetrainMotors[FRONT_LEFT] = new TalonSRX(FRONT_LEFT_TALON_ID);
+	drivetrainMotors[REAR_LEFT] = new TalonSRX(REAR_LEFT_TALON_ID);
+	drivetrainMotors[FRONT_RIGHT] = new TalonSRX(FRONT_RIGHT_TALON_ID);
+	drivetrainMotors[REAR_RIGHT] = new TalonSRX(REAR_RIGHT_TALON_ID);
+	resetEncoderValue(LEFT_ENCODER);
+	resetEncoderValue(RIGHT_ENCODER);
+	
+	
+
+	
+}
+
+ 
+public void InitializeDriveTrainMotors()
+{
+
+	chassis = new pid_DifferentialDrive(drivetrainMotors[FRONT_LEFT], drivetrainMotors[REAR_LEFT],  
+			drivetrainMotors[FRONT_RIGHT],	drivetrainMotors[REAR_RIGHT], pid_DifferentialDrive.Mode.STRAIGHT);
+	
+	straightController = new PIDController(.1, 0, 0, gyro, chassis);
+	straightController.setSetpoint(0);
+	
+	drivetrainMotors[FRONT_LEFT].setInverted(LEFT_SIDE_INVERTED);
+	drivetrainMotors[FRONT_RIGHT].setInverted(RIGHT_SIDE_INVERTED);
+	drivetrainMotors[REAR_LEFT].setInverted(LEFT_SIDE_INVERTED);
+	drivetrainMotors[REAR_RIGHT].setInverted(RIGHT_SIDE_INVERTED);
+	
+	
+}
+
+public void InitailzeEncoders()
+{
+	/*
+	drivetrainMotors[RIGHT_ENCODER].configEncoderCodesPerRev(ENCODER_COUNT);
+	drivetrainMotors[LEFT_ENCODER].configEncoderCodesPerRev(ENCODER_COUNT);
+	
+	drivetrainMotors[RIGHT_ENCODER].reverseFeedbackDevice(LEFT_ENCODER_REVERSED);
+	drivetrainMotors[LEFT_ENCODER].reverseSensor(RIGHT_ENCODER_REVERSED);
+	
+	drivetrainMotors[LEFT_ENCODER].setFeedbackDevice(FeedbackDevice.QuadEncoder);
+	drivetrainMotors[RIGHT_ENCODER].setFeedbackDevice(FeedbackDevice.QuadEncoder);
+	
+	drivetrainMotors[LEFT_ENCODER].setStatusFrameRateMs(StatusFrameRate.QuadEncoder, 10);
+	drivetrainMotors[RIGHT_ENCODER].setStatusFrameRateMs(StatusFrameRate.QuadEncoder, 10);
+	*/
+	
+	for(int i = LEFT_ENCODER; i <=  RIGHT_ENCODER; i += 2)
+	{
+		drivetrainMotors[i].configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 10);
+		drivetrainMotors[i].setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 10, 10);
+	}
+	
+	
+}
+
+public double getEncoderValue(int side)
+{
+	return ((drivetrainMotors[side].getSelectedSensorPosition() / 1024) * ((side == LEFT_ENCODER) ? LEFT_ENCODER_REVERSED : RIGHT_ENCODER_REVERSED));
+}
+
+public void resetEncoderValue(int side)
+{
+	drivetrainMotors[side].setSelectedSensorPosition(0, 10);
+}
+
+public void DriveToDistance()
+{
+	
+}
+
+public void TankDrive(double left, double right)
+{
+	chassis.tankDrive(left, right);
+}
+
+}
+
